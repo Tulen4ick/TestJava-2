a12889950c4fa693045831fa591b4bee4d80801f
anton <ashtanyuk@gmail.com>
2024-04-08 17:27:27 +0300

    Добавлен maven.yml

--- src/test/java/org/example/PrimeTest.java
+++ src/test/java/org/example/PrimeTest.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import org.junit.Assert;
+
 import static org.junit.Assert.*;
 
 public class PrimeTest {
 
     @org.junit.Test
     public void isPrime() {
+        Assert.assertEquals(false, Prime.isPrime(12));
     }
 }
